df97d1508ab46f7f560d425c4405a960ba274a4f
suprilkumar <suprilkumar892@gmail.com>
2026-05-07 02:38:02 +0530

    first stage to production - yml added

--- main/settings.py
+++ main/settings.py
@@ -1,27 +1,35 @@
 import os
 from pathlib import Path
+import environ
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+# Initialize environment variables
+env = environ.Env()
+environ.Env.read_env(os.path.join(BASE_DIR, '.env')) 
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-tq=hv)6@!p^qux7a=$e6m^kixq9%ve8+bt^@8(0@4u8$9o1^b)"
+#SECRET_KEY = "django-insecure-tq=hv)6@!p^qux7a=$e6m^kixq9%ve8+bt^@8(0@4u8$9o1^b)"
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
+DEBUG = os.environ.get('DEBUG', 'False') == 'True'
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '').split(',')
+
 
 CSRF_TRUSTED_ORIGINS = ['http://localhost:8000', 'http://127.0.0.1:8000']
 
 # For production, also add these
-SESSION_COOKIE_SECURE = True  # Only send cookies over HTTPS
-CSRF_COOKIE_SECURE = True  # Only send CSRF cookies over HTTPS
-
+SESSION_COOKIE_SECURE = not DEBUG
+CSRF_COOKIE_SECURE = not DEBUG
 
 
 # Application definition
@@ -34,7 +42,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
 
-    'whitenoise.runserver_nostatic', 
+    #'whitenoise.runserver_nostatic', 
 
     'account',
     'course',
@@ -140,3 +148,9 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 LOGIN_URL = 'account:login'
 LOGIN_REDIRECT_URL = 'dashboard:index'
 LOGOUT_REDIRECT_URL = 'public:home'
+
+WHITENOISE_AUTOREFRESH = True
+WHITENOISE_ROOT = None  # don't serve from root
+
+# Tell whitenoise to ignore media path
+WHITENOISE_SKIP_COMPRESS_EXTENSIONS = []
